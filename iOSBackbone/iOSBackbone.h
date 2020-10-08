95c7213133774ce366a1c6352d06d4d8f0da838e
giorgy gunawan <giorgy@styletheory.co>
2020-10-08 15:12:53 +0700

    Initial Commit

---
+++ iOSBackbone/iOSBackbone.h
@@ -0,0 +1,19 @@
+//
+//  iOSBackbone.h
+//  iOSBackbone
+//
+//  Created by Style Theory on 8/10/20.
+//  Copyright © 2020 joji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSBackbone.
+FOUNDATION_EXPORT double iOSBackboneVersionNumber;
+
+//! Project version string for iOSBackbone.
+FOUNDATION_EXPORT const unsigned char iOSBackboneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSBackbone/PublicHeader.h>
+
+
